82da087b8c00d1e19688e556546d411f9dabc026
Kévin Seroux <kevin.seroux@gmail.com>
2016-03-25 15:35:53 +0100

    Squelette de classe implémenté

--- DI3_Projet3_Matrices/Matrix.h
+++ DI3_Projet3_Matrices/Matrix.h
@@ -1,14 +1,44 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <iostream>
+
 template <class T>
 class CMatrix
 {
 public:
 	CMatrix(unsigned int uiCountRows, unsigned int uiCountColumns);
+
 	CMatrix<T> operator+(const CMatrix<T>&) const;
+	CMatrix<T>& operator+=(const CMatrix<T>&);
+
+	CMatrix<T> operator-(const CMatrix<T>&) const;
+	CMatrix<T>& operator-=(const CMatrix<T>&);
+
+	CMatrix<T> operator*(const CMatrix<T>&) const;
+	CMatrix<T>& operator*=(const CMatrix<T>&);
+	CMatrix<T> operator*(const T) const;
+	CMatrix<T>& operator*=(const T);
+
+	CMatrix<T> operator/(const CMatrix<T>&) const;
+	CMatrix<T>& operator/=(const CMatrix<T>&);
+	CMatrix<T> operator/(const T) const;
+	CMatrix<T>& operator/=(const T);
+
+	CMatrix<T> transpose();
+
 	CMatrix<T>& operator=(const CMatrix<T>&);
+
+	bool operator==(const CMatrix<T>&);
+
+	T operator()(unsigned int uiRow, unsigned int uiColumn);
+
+	std::ostream& operator<<(std::ostream& stream);
 };
+
+//Free functions
+template <class T> CMatrix<T> operator*(const T, const CMatrix<T>&);
+template <class T> CMatrix<T> operator/(const T, const CMatrix<T>&);
 
 #include "Matrix.hpp"
 
--- DI3_Projet3_Matrices/Matrix.hpp
+++ DI3_Projet3_Matrices/Matrix.hpp
@@ -1,17 +1,150 @@
-template <class T>
-CMatrix<T>::CMatrix(const unsigned int uiCountRows, const unsigned int uiCountColumns)
-{
+#include <cassert>
 
+template <class T>
+CMatrix<T>::CMatrix(unsigned int uiCountRows, unsigned int uiCountColumns)
+{
+	assert(false && "Not Implemented Yet");
 }
+
 
 template <class T>
 CMatrix<T> CMatrix<T>::operator+(const CMatrix<T>&) const
 {
+	assert(false && "Not Implemented Yet");
+	return CMatrix<T>(1,1);
+}
+
+template <class T>
+CMatrix<T>& CMatrix<T>::operator+=(const CMatrix<T>&)
+{
+	assert(false && "Not Implemented Yet");
+	return this;
+}
+
+
+template <class T>
+CMatrix<T> CMatrix<T>::operator-(const CMatrix<T>&) const
+{
+	assert(false && "Not Implemented Yet");
+	return CMatrix<T>(1,1);
+}
+
+template <class T>
+CMatrix<T>& CMatrix<T>::operator-=(const CMatrix<T>&)
+{
+	assert(false && "Not Implemented Yet");
+	return this;
+}
+
+
+template <class T>
+CMatrix<T> CMatrix<T>::operator*(const CMatrix<T>&) const
+{
+	assert(false && "Not Implemented Yet");
+	return CMatrix<T>(1,1);
+}
+
+template <class T>
+CMatrix<T>& CMatrix<T>::operator*=(const CMatrix<T>&)
+{
+	assert(false && "Not Implemented Yet");
+	return this;
+}
+
+template <class T>
+CMatrix<T> CMatrix<T>::operator*(const T) const
+{
+	assert(false && "Not Implemented Yet");
+	return CMatrix<T>(1,1);
+}
+
+template <class T>
+CMatrix<T>& CMatrix<T>::operator*=(const T)
+{
+	assert(false && "Not Implemented Yet");
+	return this;
+}
+
+
+template <class T>
+CMatrix<T> CMatrix<T>::operator/(const CMatrix<T>&) const
+{
+	assert(false && "Not Implemented Yet");
+	return CMatrix<T>(1,1);
+}
+
+template <class T>
+CMatrix<T>& CMatrix<T>::operator/=(const CMatrix<T>&)
+{
+	assert(false && "Not Implemented Yet");
+	return this;
+}
+
+template <class T>
+CMatrix<T> CMatrix<T>::operator/(const T) const
+{
+	assert(false && "Not Implemented Yet");
+	return CMatrix<T>(1,1);
+}
+
+template <class T>
+CMatrix<T>& CMatrix<T>::operator/=(const T)
+{
+	assert(false && "Not Implemented Yet");
+	return this;
+}
+
+
+template <class T>
+CMatrix<T> CMatrix<T>::transpose()
+{
+	assert(false && "Not Implemented Yet");
 	return CMatrix<T>(1,1);
 }
 
 template <class T>
 CMatrix<T>& CMatrix<T>::operator=(const CMatrix<T>&)
 {
+	assert(false && "Not Implemented Yet");
 	return this;
+}
+
+
+template <class T>
+bool CMatrix<T>::operator==(const CMatrix<T>&)
+{
+	assert(false && "Not Implemented Yet");
+	return true;
+}
+
+
+template <class T>
+T CMatrix<T>::operator()(unsigned int uiRow, unsigned int uiColumn)
+{
+	assert(false && "Not Implemented Yet");
+	return T;
+}
+
+
+template <class T>
+std::ostream& CMatrix<T>::operator<<(std::ostream& stream)
+{
+	assert(false && "Not Implemented Yet");
+	return stream;
+}
+
+
+//Free functions
+template <class T>
+CMatrix<T> operator*(const T, const CMatrix<T>&)
+{
+	assert(false && "Not Implemented Yet");
+	return CMatrix<T>(1,1);
+}
+
+template <class T>
+CMatrix<T> operator/(const T, const CMatrix<T>&)
+{
+	assert(false && "Not Implemented Yet");
+	return CMatrix<T>(1,1);
 }
