65156c52b556144c830d3f236d15a5972a1f63e1
Julien Amoros <ju.amoros@laposte.net>
2016-03-26 15:55:22 +0100

    rédaction classe Cexception

--- DI3_Projet3_Matrices/Cexception.cpp
+++ DI3_Projet3_Matrices/Cexception.cpp
@@ -1,11 +1,54 @@
+/**************************************************
+Titre: Classe pour la gestion des exceptions
+***************************************************
+Corps de la Classe Cexception
+Permet de créer des objets remontés lors de 
+la levée d'exceptions.
+**************************************************/
+
 #include "Cexception.h"
 
-
+/**************************************************
+* CException
+* *************************************************
+* Constructeur par défaut
+* *************************************************
+* Entrée:
+* Pré-condition :
+* Sortie :
+* Post-condition : Exception initialisé à 0
+* ************************************************/
 Cexception::Cexception(void)
 {
+	uiEXCvaleur = 0;
 }
 
-
-Cexception::~Cexception(void)
+/**************************************************
+* Excmodifier_valeur
+* *************************************************
+* Modifier valeur de l'exception
+* *************************************************
+* Entrée: nouvelle valeure
+* Pré-condition :
+* Sortie :
+* Post-condition : valeure de l'exception modifiée
+* ************************************************/
+void Cexception::EXCmodifier_valeur(unsigned int val)
 {
+	uiEXCvaleur = val;
+}
+
+/**************************************************
+* EXClire_valeur
+* *************************************************
+* Permet de lire la valeur de l'exception
+* *************************************************
+* Entrée:
+* Pré-condition :
+* Sortie : valeur de l'exception
+* Post-condition : valeur de l'exception retournée
+* ************************************************/
+unsigned int Cexception::EXClire_valeur()
+{
+	return uiEXCvaleur;
 }
--- DI3_Projet3_Matrices/Cexception.h
+++ DI3_Projet3_Matrices/Cexception.h
@@ -1,8 +1,70 @@
-#pragma once
+#ifndef CEXCEPTION_H
+#define CEXCEPTION_H
+
+/**************************************************
+Titre: Classe pour la gestion des exceptions
+***************************************************
+Interface de la Classe Cexception
+Permet de créer des objets remontés lors de 
+la levée d'exceptions.
+**************************************************/
+
 class Cexception
 {
+	//Attribut
+private:
+	unsigned int uiEXCvaleur; //Contient la valeur de l'exception
+
+	//Constructeur et destructeur
 public:
+/**************************************************
+* CException
+* *************************************************
+* Constructeur par défaut
+* *************************************************
+* Entrée:
+* Pré-condition :
+* Sortie :
+* Post-condition : Exception initialisé à 0
+* ************************************************/
 	Cexception(void);
+
+/**************************************************
+* ~CException
+* *************************************************
+* Destructeur de la classe
+* *************************************************
+* Entrée:
+* Pré-condition :
+* Sortie :
+* Post-condition : exception détruite
+* ************************************************/
 	~Cexception(void);
+
+	//Methode
+/**************************************************
+* Excmodifier_valeur
+* *************************************************
+* Modifier valeur de l'exception
+* *************************************************
+* Entrée: nouvelle valeure
+* Pré-condition :
+* Sortie :
+* Post-condition : valeure de l'exception modifiée
+* ************************************************/
+	void EXCmodifier_valeur(unsigned int);
+
+/**************************************************
+* EXClire_valeur
+* *************************************************
+* Permet de lire la valeur de l'exception
+* *************************************************
+* Entrée:
+* Pré-condition :
+* Sortie : valeur de l'exception
+* Post-condition : valeur de l'exception retournée
+* ************************************************/
+	unsigned int EXClire_valeur();
 };
 
+#endif
