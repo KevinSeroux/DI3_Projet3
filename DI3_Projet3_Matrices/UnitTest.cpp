a81999a07c2bee21ed444fbb3fccbf552280f97b
Julien Amoros <ju.amoros@laposte.net>
2016-03-30 15:57:08 +0200

    modification placement tests unitaires

--- DI3_Projet3_Matrices/UnitTest.cpp
+++ DI3_Projet3_Matrices/UnitTest.cpp
@@ -41,30 +41,6 @@ static void testMatrixConstructorByCopy()
 	assert(MATCopy(2, 1) == 32);
 }
 
-static void testMatrixOperatorEqual()
-{
-	CMatrix<int> MAT(2, 2);
-	MAT(0, 0) = 11;
-	MAT(0, 1) = 12;
-	MAT(1, 0) = 21;
-	MAT(1, 1) = 22;
-
-	CMatrix<int> MATCopy(2,2);
-	MATCopy = MAT;
-
-	assert(MATCopy(0, 0) == 11);
-	assert(MATCopy(0, 1) == 12);
-	assert(MATCopy(1, 0) == 21);
-	assert(MATCopy(1, 1) == 22);
-}
-
-static void testMatrixOperatorParenthesis()
-{
-	CMatrix<int> MAT(2, 3);
-	MAT(1,2) = 43;
-	assert(MAT(1,2) == 43);
-}
-
 static void testMatrixOperatorAdd()
 {
 	CMatrix<int> MAT1(2, 2);
@@ -95,6 +71,43 @@ static void testMatrixOperatorAdd()
 	assert(MAT(1, 0) == 342);
 	assert(MAT(1, 1) == 344);
 }
+
+static void testMatrixOperatorAddEqual()
+{
+	CMatrix<int> MAT1(2, 2);
+	MAT1(0, 0) = 111;
+	MAT1(0, 1) = 112;
+	MAT1(1, 0) = 121;
+	MAT1(1, 1) = 122;
+
+	CMatrix<int> MAT2(2, 2);
+	MAT2(0, 0) = 211;
+	MAT2(0, 1) = 212;
+	MAT2(1, 0) = 221;
+	MAT2(1, 1) = 222;
+
+	CMatrix<int> MAT(2, 2);
+	MAT(0, 0) = 0;
+	MAT(0, 1) = 0;
+	MAT(1, 0) = 0;
+	MAT(1, 1) = 0;
+	MAT += MAT1 += MAT2;
+
+	assert(MAT(0, 0) == 322);
+	assert(MAT(0, 1) == 324);
+	assert(MAT(1, 0) == 342);
+	assert(MAT(1, 1) == 344);
+
+	CMatrix<int> MAT3(3, 2);
+
+	try
+	{
+	    MAT1 += MAT3;
+	    assert(false);
+	}
+	catch(Cexception& e){ cout << "exception catched";}
+}
+
 static void testMatrixOperatorSub()
 {
 	CMatrix<int> MAT1(2, 2);
@@ -138,6 +151,45 @@ static void testMatrixOperatorSubEqual()
 	assert(MAT(0, 1) == -1);
 	assert(MAT(1, 0) == 1);
 	assert(MAT(1, 1) == 3);
+}
+
+static void testMatrixOperatorMultMat()
+{
+    CMatrix<int> O(2, 2);
+	O(0, 0) = 0;
+	O(0, 1) = 0;
+	O(1, 0) = 0;
+	O(1, 1) = 0;
+
+	CMatrix<int> I(2, 2);
+	I(0, 0) = 1;
+	I(0, 1) = 0;
+	I(1, 0) = 0;
+	I(1, 1) = 1;
+
+	CMatrix<int> MAT(2, 2);
+	MAT(0, 0) = 11;
+	MAT(0, 1) = 12;
+	MAT(1, 0) = 21;
+	MAT(1, 1) = 22;
+
+    CMatrix<int> MATLig(1, 3); //tester avec matrices tailles !=
+	MATLig(0, 0) = 1;
+	MATLig(0, 1) = 2;
+	MATLig(0, 2) = 1;
+
+	CMatrix<int> MATCol(3, 1);
+	MATCol(0, 0) = 2;
+	MATCol(1, 0) = 4;
+	MATCol(2, 0) = 8;
+
+	//cout << (MATLig * MATCol);
+	//cout << (MATCol * MATLig);
+
+	if(!((MAT * O) == O))
+        assert(false);
+    if(!((MAT * I) == MAT))
+        assert(false);
 }
 
 static void testMatrixOperatorDiv()
@@ -212,96 +264,21 @@ static void testMatrixOperatorDivEqualExc()
 	};
 }
 
-static void testMatrixOperatorAddEqual()
+static void testMatrixOperatorEqual()
 {
-	CMatrix<int> MAT1(2, 2);
-	MAT1(0, 0) = 111;
-	MAT1(0, 1) = 112;
-	MAT1(1, 0) = 121;
-	MAT1(1, 1) = 122;
-
-	CMatrix<int> MAT2(2, 2);
-	MAT2(0, 0) = 211;
-	MAT2(0, 1) = 212;
-	MAT2(1, 0) = 221;
-	MAT2(1, 1) = 222;
-
-	CMatrix<int> MAT(2, 2);
-	MAT(0, 0) = 0;
-	MAT(0, 1) = 0;
-	MAT(1, 0) = 0;
-	MAT(1, 1) = 0;
-	MAT += MAT1 += MAT2;
-
-	assert(MAT(0, 0) == 322);
-	assert(MAT(0, 1) == 324);
-	assert(MAT(1, 0) == 342);
-	assert(MAT(1, 1) == 344);
-
-	CMatrix<int> MAT3(3, 2);
-
-	try
-	{
-	    MAT1 += MAT3;
-	    assert(false);
-	}
-	catch(Cexception& e){ cout << "exception catched";}
-}
-
-static void testMatrixOperatorStream()
-{
-    CMatrix<int> MAT1(2, 2);
-	MAT1(0, 0) = 111;
-	MAT1(0, 1) = 112;
-	MAT1(1, 0) = 121;
-	MAT1(1, 1) = 122;
-	cout << MAT1;
-
-	CMatrix<double> MAT2(2, 2);
-	MAT2(0, 0) = .111;
-	MAT2(0, 1) = 1.12;
-	MAT2(1, 0) = 12.1;
-	MAT2(1, 1) = 122.;
-	cout << MAT2;
-}
-
-static void testMatrixOperatorMultMat()
-{
-    CMatrix<int> O(2, 2);
-	O(0, 0) = 0;
-	O(0, 1) = 0;
-	O(1, 0) = 0;
-	O(1, 1) = 0;
-
-	CMatrix<int> I(2, 2);
-	I(0, 0) = 1;
-	I(0, 1) = 0;
-	I(1, 0) = 0;
-	I(1, 1) = 1;
-
 	CMatrix<int> MAT(2, 2);
 	MAT(0, 0) = 11;
 	MAT(0, 1) = 12;
 	MAT(1, 0) = 21;
 	MAT(1, 1) = 22;
 
-    CMatrix<int> MATLig(1, 3); //tester avec matrices tailles !=
-	MATLig(0, 0) = 1;
-	MATLig(0, 1) = 2;
-	MATLig(0, 2) = 1;
+	CMatrix<int> MATCopy(2,2);
+	MATCopy = MAT;
 
-	CMatrix<int> MATCol(3, 1);
-	MATCol(0, 0) = 2;
-	MATCol(1, 0) = 4;
-	MATCol(2, 0) = 8;
-
-	cout << (2 * MATCol);
-	cout << (MATCol * 2);
-
-	if(!((MAT * O) == O))
-        assert(false);
-    if(!((MAT * I) == MAT))
-        assert(false);
+	assert(MATCopy(0, 0) == 11);
+	assert(MATCopy(0, 1) == 12);
+	assert(MATCopy(1, 0) == 21);
+	assert(MATCopy(1, 1) == 22);
 }
 
 static void testMatrixOperatorEquality()
@@ -333,6 +310,30 @@ static void testMatrixOperatorEquality()
     {
         //Gestion de l'exception
     }
+}
+
+static void testMatrixOperatorParenthesis()
+{
+	CMatrix<int> MAT(2, 3);
+	MAT(1,2) = 43;
+	assert(MAT(1,2) == 43);
+}
+
+static void testMatrixOperatorStream()
+{
+    CMatrix<int> MAT1(2, 2);
+	MAT1(0, 0) = 111;
+	MAT1(0, 1) = 112;
+	MAT1(1, 0) = 121;
+	MAT1(1, 1) = 122;
+	cout << MAT1;
+
+	CMatrix<double> MAT2(2, 2);
+	MAT2(0, 0) = .111;
+	MAT2(0, 1) = 1.12;
+	MAT2(1, 0) = 12.1;
+	MAT2(1, 1) = 122.;
+	cout << MAT2;
 }
 
 void CUnitTest::testMatrix()
